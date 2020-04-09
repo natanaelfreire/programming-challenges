ec86b85f25f90d3487a7eb18a21f6dd7f8cc1e88
Natanael Freire <natannascimento1@hotmail.com>
2020-04-09 01:12:56 -0300

    delete unnecessary files

--- Minesweeper/minesweeper.c
+++ Minesweeper/minesweeper.c
@@ -1,81 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char ** criaMatriz(int n, int m){
+char ** criaMatriz(int n, int m) {
 	int i;
 
 	char **mat;
 	mat = malloc(sizeof(char *) * n);
-	for(i = 0; i < n; ++i){
+
+	for (i = 0; i < n; ++i) {
 		mat[i] = malloc(sizeof(char) * m);
 	}
 
 	return mat;
 }
 
-void printField(char **mat, int n, int m){
+void printField(char **mat, int n, int m) {
 	int cnt, i, j;
 
-	for(i = 0; i < n; ++i){
-		for(j = 0; j < m; ++j){
-			if(mat[i][j] == '*')
+	for (i = 0; i < n; ++i) {
+		for (j = 0; j < m; ++j) {
+			if (mat[i][j] == '*')
 				printf("*");
-			else{
+			else {
 				cnt = 0;
-				if(i != 0 && j != 0)
-					if(mat[i-1][j-1] == '*')
+				if (i != 0 && j != 0)
+					if (mat[i-1][j-1] == '*')
 						cnt++;
 
-				if(i != 0)
-					if(mat[i-1][j] == '*')
+				if (i != 0)
+					if (mat[i-1][j] == '*')
 						cnt++;
 
-				if(i != 0 && j != m-1)
-					if(mat[i-1][j+1] == '*')
+				if (i != 0 && j != m-1)
+					if (mat[i-1][j+1] == '*')
 						cnt++;
 
-				if(j != 0)
-					if(mat[i][j-1] == '*')
+				if (j != 0)
+					if (mat[i][j-1] == '*')
 						cnt++;
 
-				if(j != m-1)
-					if(mat[i][j+1] == '*')
+				if (j != m-1)
+					if (mat[i][j+1] == '*')
 						cnt++;
 
-				if(i != n-1 && j != 0)
-					if(mat[i+1][j-1] == '*')
+				if (i != n-1 && j != 0)
+					if (mat[i+1][j-1] == '*')
 						cnt++;
 
-				if(i != n-1)
-					if(mat[i+1][j] == '*')
+				if (i != n-1)
+					if (mat[i+1][j] == '*')
 						cnt++;
 
-				if(i != n-1 && j != m-1)
-					if(mat[i+1][j+1] == '*')
+				if (i != n-1 && j != m-1)
+					if (mat[i+1][j+1] == '*')
 						cnt++;	
 
 				printf("%d", cnt);
-
 			}
 		}
+
 		printf("\n");
-
 	}
-
 }
 
-
-int main(){
-
+int main() {
 	int n, m, i, j, cnt = 1;
 
 	scanf("%d %d\n", &n, &m);
-	while(n != 0 && m != 0){
-		if(n > 0 && m <= 100){
+	while (n != 0 && m != 0) {
+		if (n > 0 && m <= 100) {
 			char **field = criaMatriz(n, m);
 
-			for(i = 0; i < n; ++i){
-				for(j = 0; j < m; ++j)
+			for (i = 0; i < n; ++i) {
+				for (j = 0; j < m; ++j)
 					scanf("%c", &field[i][j]);
 				scanf("\n");
 			}
@@ -84,12 +81,11 @@ int main(){
 			printField(field, n, m);
 
 			scanf("%d %d\n", &n, &m);
-			if(n == 0 && m == 0) return 0;
+			if (n == 0 && m == 0) return 0;
 			cnt++;
 			printf("\n");
 		}
 	}
-
 
 	return 0;
 }
--- The3nPlus1Problem/The3nPlus1Problem.c
+++ The3nPlus1Problem/The3nPlus1Problem.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 
-int sequenceOfN(int n){
+int sequenceOfN(int n) {
 	int cnt = 1;
-	while(n != 1){
-		if(n % 2 == 0)
+
+	while (n != 1) {
+		if (n % 2 == 0)
 			n = n/2;
 		else
 			n = n*3 + 1;
+
 		cnt++;
 	}
+
 	return cnt;
 }
 
-int main()
-{
+int main() {
 	int n, i, j, cmp, r;
 
-	while(scanf("%d %d", &i, &j)!=EOF){
-		if(i < 1000000 && i > 0 && j < 1000000 && j > 0){
+	while (scanf("%d %d", &i, &j) != EOF) {
+		if (i < 1000000 && i > 0 && j < 1000000 && j > 0) {
 			cmp = 0;
-			if(i < j){
-				for(n = i; n <= j; n++){
+			if (i < j) {
+				for (n = i; n <= j; n++) {
 					r = sequenceOfN(n);
-					if(r > cmp)
+					if (r > cmp)
 						cmp = r;
 				}
-			}else{
-				for(n = j; n <= i; n++){
+			} else {
+				for (n = j; n <= i; n++) {
 					r = sequenceOfN(n);
-					if(r > cmp)
+					if (r > cmp)
 						cmp = r;
 				}
 			}
@@ -36,5 +38,6 @@ int main()
 			printf("%d %d %d\n", i, j, cmp); 
 		}
 	}
+
 	return 0;
 }
